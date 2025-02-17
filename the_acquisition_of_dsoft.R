459d057d81bd93a654e1eb15b7a8e0190d142043
Sangho Lee (Stan) <sanlee8495@gmail.com>
2025-02-16 17:29:31 -0800

    02162025

---
+++ the_acquisition_of_dsoft.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(DT)
+library(rmarkdown)
+library(shinyjs)
+
+ui <- fluidPage(
+  id = "main_nav",
+  title = (""),
+  tabPanel("",
+           fluidPage(
+             uiOutput("chapter")))
+)
+
+server <- function(input, output, session) {
+  output$chapter <- renderUI({
+    file_name <- "the_acquisition_of_dsoft.html" 
+    if (!is.null(file_name)) {
+      tags$iframe(src = file_name, style = "width:100%; height:800px;")
+    }
+  })
+}
+
+shinyApp(ui = ui, server = server)
